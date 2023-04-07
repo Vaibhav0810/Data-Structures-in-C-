fdf86ab6a0c7013c251e817818dfabdac13895df
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2023-04-07 16:16:43 +0530

    Update code.cpp

--- dp/longest common subsequence variations/Longest Common Substring/code.cpp
+++ dp/longest common subsequence variations/Longest Common Substring/code.cpp
@@ -1,21 +1,24 @@
-int recur(int n,int m, string s1, string s2){
-        
-        if(n==0 || m==0) return 0;
-        
-        if(s1[n-1]==s2[m-1]){
-            return 1+recur(n-1,m-1,s1,s2);
-        }
-        else{
-            return 0;
-        }
-        return recur(n-1,m,s1,s2);
-            return recur(n,m-1,s1,s2);
-        
-    }
-    
-    int longestCommonSubstr (string S1, string S2, int n, int m)
+int longestCommonSubstr (string S1, string S2, int n, int m)
     {
         
-        return recur(n,m,S1,S2);
+        int dp[n+1][m+1]= {0};
+        int ans=0;
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(i==0 || j==0) dp[i][j]=0;
+            }
+        }
         
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(S1[i-1]==S2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                    ans=max(ans,dp[i][j]);
+                }
+                else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+        return ans;
     }
